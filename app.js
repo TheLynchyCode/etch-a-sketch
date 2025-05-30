fe0be45ab4371e7c8a11233c8e108c6f153ef22c
90117249+TheLynchyCode@users.noreply.github.com <90117249+TheLynchyCode@users.noreply.github.com>
2025-05-30 12:38:26 +1000

    update the number of iterations to 256 in the for loop

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('div');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 1; i <= 256; i++) {
   const divEl = document.createElement('div');
   divEl.textContent = i;
   container.appendChild(divEl);

7dbae406c18ab4c9500109c8101690a47dde056e
90117249+TheLynchyCode@users.noreply.github.com <90117249+TheLynchyCode@users.noreply.github.com>
2025-05-30 13:01:28 +1000

    Create array of divs and add event listeners
    convert nodelist to an array and run a for of loop adding mouseover event listeners to each div

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ for (let i = 1; i <= 256; i++) {
   divEl.textContent = i;
   container.appendChild(divEl);
 }
+
+// NodeList of divs
+const childDivs = document.querySelectorAll('.container > div');
+// Conversion of NodeList to an Array
+const childDivsArray = Array.from(childDivs);
+
+// Iterating through div array, adding an event listener to each div, changing background color on mouseover event
+for (let i of childDivsArray) {
+  i.addEventListener('mouseover', function () {
+    i.setAttribute('style', 'background-color: #333');
+  });
+}

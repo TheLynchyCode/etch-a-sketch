d0497a1f8adb7dd527a881e8701b9ad2e5192cd1
90117249+TheLynchyCode@users.noreply.github.com <90117249+TheLynchyCode@users.noreply.github.com>
2025-06-06 14:28:10 +1000

    Add a user select grid size prompt

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const container = document.querySelector('div');
+const container = document.querySelector('.container');
+const sizeBtn = document.querySelector('#sizeBtn');
 
-for (let i = 1; i <= 256; i++) {
-  const divEl = document.createElement('div');
-  divEl.textContent = i;
-  container.appendChild(divEl);
-}
+// create 16 x 16 grid upon page loading
+window.addEventListener('DOMContentLoaded', function () {
+  for (let i = 1; i <= 256; i++) {
+    const divEl = document.createElement('div');
+    divEl.textContent = i;
+    divEl.classList.add('active');
+    divEl.addEventListener('mouseenter', function () {
+      divEl.classList.add('nonactive');
+    });
+    container.appendChild(divEl);
+  }
+});
 
-// NodeList of divs
-const childDivs = document.querySelectorAll('.container > div');
-// Conversion of NodeList to an Array
-const childDivsArray = Array.from(childDivs);
-
-// Iterating through div array, adding an event listener to each div, changing background color on mouseover event
-for (let i of childDivsArray) {
-  i.addEventListener('mouseover', function () {
-    i.setAttribute('style', 'background-color: #333');
-  });
-}
+// user select grid size
+sizeBtn.addEventListener('click', function () {
+  prompt('Enter a number to determine grid size');
+});
